97ac3c2a9f42d3f274d1d70e0e91add4953e23d8
ergul13 <152935337+ergul13@users.noreply.github.com>
2025-05-19 12:10:49 +0300

    yardım lazım

--- src/main/java/model/TrafficCalculator.java
+++ src/main/java/model/TrafficCalculator.java
@@ -23,6 +23,7 @@ public class TrafficCalculator {
 
     private final Random random = new Random();
 
+    //yardım lazım
     public void setInitialCounts(Integer north, Integer south, Integer east, Integer west) {
         // Kullanıcıdan veri gelmezse rastgele 0-25 ata
         northCount = (north != null) ? north : random.nextInt(26);
